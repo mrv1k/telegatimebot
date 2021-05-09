d36ceb73993083a2cb8507071317f5ca9eaba2df
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-05-09 18:16:20 -0400

    feat: listen to youtube urls in messages

--- src/bot.js
+++ src/bot.js
@@ -1,17 +1,41 @@
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
 
 const { Telegraf } = require("telegraf");
+const urlParser = require("js-video-url-parser/lib/base");
+require("js-video-url-parser/lib/provider/youtube");
 
 const fetchDuration = require("./fetchDuration");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("WIP"));
-bot.on("sticker", async (ctx) => {
-  const duration = await fetchDuration();
-  ctx.reply(`Video length is ${duration.minutes}`);
+
+// regex tests - https://regexr.com/5si73
+bot.url(/youtu(\.)?be/, async (ctx) => {
+  const url = ctx.match.input;
+  // Parsing an incorrect url or trying to create one with an invalid object will return undefined
+  const parsed = urlParser.parse(url);
+  if (parsed === undefined) {
+    return ctx.reply("Could not parse the YouTube's url");
+  }
+
+  const duration = await fetchDuration(parsed.id);
+  ctx.reply(JSON.stringify(duration));
 });
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+
+bot.command("stfu", (ctx) => {
+  // TODO: silence mode
+});
+bot.command("unstfu", (ctx) => {
+  // TODO: disable silenec mode
+});
+bot.command("gtfo", (ctx) => {
+  ctx.reply("Self-destruct initiated");
+  setTimeout(() => {
+    ctx.leaveChat();
+  }, 1000);
+});
+
 bot.launch();
 
 console.log("I am ALIVE!");
