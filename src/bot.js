ae07d1c99b405da7d234fe3696a1e23baf31126d
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-05-09 21:55:39 -0400

    feat: add /duration command

--- src/bot.js
+++ src/bot.js
@@ -9,22 +9,17 @@ const formatDuration = require("./formatDuration");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("WIP"));
+bot.help((ctx) =>
+  ctx.reply(`
+Get video duration:
+/duration <url>
+`)
+);
 
 // regex tests - https://regexr.com/5si73
-bot.url(/youtu(\.)?be/, async (ctx) => {
+bot.url(/youtu(\.)?be/, (ctx) => {
   const url = ctx.match.input;
-  // Parsing an incorrect url or trying to create one with an invalid object will return undefined
-  const parsed = urlParser.parse(url);
-  if (parsed === undefined) {
-    return ctx.reply("Could not parse the YouTube's url");
-  }
-
-  const duration = await fetchDuration(parsed.id);
-  const formattedDuration = formatDuration(duration);
-  ctx.reply(`Duration: ${formattedDuration}`, {
-    reply_to_message_id: ctx.message.message_id,
-  });
+  sendDurationReply(ctx, url);
 });
 
 bot.command("stfu", (ctx) => {
@@ -40,6 +35,25 @@ bot.command("gtfo", (ctx) => {
   }, 1000);
 });
 
+bot.command(["length", "duration"], async (ctx) => {
+  const params = ctx.message.text.split(" ");
+  if (params.length === 1) {
+    const rick_url = "https://youtu.be/oHg5SJYRHA0";
+    await ctx.reply("Gets youtube video duration.");
+    const botMessage = await ctx.reply(`${params[0]} ${rick_url}`, {
+      disable_web_page_preview: true,
+    });
+
+    console.log(botMessage.message_id, ctx.message.message_id);
+
+    await sendDurationReply(ctx, rick_url, botMessage.message_id);
+    return;
+  }
+
+  const url = params[1];
+  await sendDurationReply(ctx, url);
+});
+
 bot.launch();
 
 console.log("I am ALIVE!");
@@ -47,3 +61,22 @@ console.log("I am ALIVE!");
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
+async function getDuration(url) {
+  // Parsing an incorrect url or trying to create one with an invalid object will return undefined
+  const parsed = urlParser.parse(url);
+  if (parsed === undefined) {
+    return ctx.reply("Could not parse the YouTube's url");
+  }
+  const duration = await fetchDuration(parsed.id);
+  return formatDuration(duration);
+}
+
+async function sendDurationReply(ctx, url, reply_id) {
+  let reply_to_message_id = reply_id ?? ctx.message.message_id;
+  const duration = await getDuration(url);
+
+  ctx.reply(`Duration: ${duration}`, {
+    reply_to_message_id,
+  });
+}
