292cddcd53c4200dcb7b220f21edb18a34cedaa7
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-05-11 17:42:29 -0400

    refactor: improve readability

--- src/formatDuration.ts
+++ src/formatDuration.ts
@@ -1,31 +1,32 @@
 import { Duration } from "iso8601-duration";
 
-const doubleDigit = (digit: number) => `${digit <= 9 ? "0" : ""}${digit}`;
+const pad0 = (digit: number) => `${digit <= 9 ? "0" : ""}${digit}`;
 
 export default function formatDuration(parsedDuration: Duration): string {
   const { days = 0, hours = 0, minutes = 0, seconds = 0 } = parsedDuration;
 
-  const none = seconds === 0 && minutes === 0 && hours === 0 && days === 0;
-  if (none) {
-    return "¯\\_(ツ)_/¯ It's a live stream";
+  const hasSeconds = seconds !== 0;
+  const hasMinutes = minutes !== 0;
+  const hasHours = hours !== 0;
+  const hasDays = days !== 0;
+
+  const hasAll = [hasSeconds, hasMinutes, hasHours, hasDays];
+  if (hasAll.every((v) => v === false)) {
+    return "will throw";
+    // throw Error("Duration: All numbers are 0");
   }
 
-  const onlySeconds =
-    seconds !== 0 && minutes === 0 && hours === 0 && days === 0;
-  if (onlySeconds) {
-    return `0:${doubleDigit(seconds)}`;
-  }
+  const onlySeconds = hasSeconds && !hasMinutes && !hasHours && !hasDays;
+  if (onlySeconds) return `0:${pad0(seconds)}`;
 
-  const hasMinutes = minutes !== 0 && hours === 0 && days === 0;
-  if (hasMinutes) {
-    return `${minutes}:${doubleDigit(seconds)}`;
-  }
+  const minutesWithSeconds = hasMinutes && !hasHours && !hasDays;
+  if (minutesWithSeconds) return `${minutes}:${pad0(seconds)}`;
 
-  const ss = doubleDigit(seconds);
-  const mm = doubleDigit(minutes);
+  const ss = pad0(seconds);
+  const mm = pad0(minutes);
   let h = 0;
-  if (hours !== 0) h += hours;
-  if (days !== 0) h += days * 24;
+  if (hasHours) h += hours;
+  if (hasDays) h += days * 24;
 
   return `${h}:${mm}:${ss}`;
 }
