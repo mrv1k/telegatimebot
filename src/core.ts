052e4e6ca7501c61f32c72b351fbc498fc024365
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-05-16 13:21:53 -0400

    refactor

--- src/core.ts
+++ src/core.ts
@@ -2,16 +2,16 @@ import urlParser from "js-video-url-parser/lib/base";
 import "js-video-url-parser/lib/provider/youtube";
 import type { VideoInfo } from "js-video-url-parser/lib/urlParser";
 
-import fetchDuration from "./fetchDuration";
+import fetchDuration from "./fetch-duration";
 import { formatTime, secondsToTime } from "./time";
 
-export function parseUrl(text: string) {
+export function parseUrl(text: string): VideoInfo {
   const parsedUrl = urlParser.parse(text);
   if (parsedUrl) return parsedUrl;
   throw Error("Could not parse YouTube's URL");
 }
 
-export function getUrlTimestamp(parsedUrl: VideoInfo) {
+export function getUrlTimestamp(parsedUrl: VideoInfo): number | undefined {
   const params = parsedUrl?.params;
 
   // url parser uses 0 if ?t param is found but failed to parse, ie: ?t=123a or ?t=-1
@@ -21,24 +21,24 @@ export function getUrlTimestamp(parsedUrl: VideoInfo) {
   }
 }
 
-export function getUrlTimestampOrThrow(parsedUrl: VideoInfo) {
+export function getUrlTimestampOrThrow(parsedUrl: VideoInfo): number {
   const parsed = getUrlTimestamp(parsedUrl);
   if (parsed) return parsed;
   throw Error("Could not get timestamp");
 }
 
-export function getTimestampText(timestamp: number) {
+export function getTimestampText(timestamp: number): string {
   const timestampText = formatTime(secondsToTime(timestamp));
   return `Timestamp: ${timestampText}`;
 }
 
-export async function getDurationText(parsedUrl: VideoInfo) {
+export async function getDurationText(parsedUrl: VideoInfo): Promise<string> {
   const duration = await fetchDuration(parsedUrl.id);
   // use underscore to display as italic with markdown renderer to break rendering as clickable timestamp
   return `Duration: _${formatTime(duration)}_`;
 }
 
-export function findFirstArg(text: string) {
+export function findFirstArg(text: string): string | false {
   const parts = text.split(/ +/);
   if (parts.length === 1) return false;
   return parts[1];
