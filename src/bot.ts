9d93e8e47640eed4426dce25344b99c7c3f18a42
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2024-03-10 23:50:47 -0400

    heroku?

--- src/bot.ts
+++ src/bot.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { Telegraf } from "telegraf";
 import commands from "./commands";
 import errorHandler from "./errors";
-import http from "http";
 import { useNewReplies } from "telegraf/future";
+import { fastify } from "fastify";
 
 const { BOT_TOKEN, BOT_USERNAME = "telegatimebot" } = process.env;
 if (BOT_TOKEN === undefined) {
@@ -20,49 +20,69 @@ const bot = new Telegraf(BOT_TOKEN);
 bot.use(useNewReplies());
 bot.catch(errorHandler);
 bot.use(commands);
+// Enable graceful stop & kill
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
 if (process.env.NODE_ENV === "debug") {
   bot.use(Telegraf.log());
 }
 
 
-if (process.env.NODE_ENV === "production") {
-  if (!process.env.WEBHOOK_DOMAIN) {
-    throw new Error('"WEBHOOK_DOMAIN" env var is required!');
-  }
-  // https://telegraf.js.org/#md:webhooks
+// bot1817589905
+const go = async () => {
+  // const app = fastify();
+
+  const WEBHOOK_DOMAIN='https://telegatimebot-6369c39fac19.herokuapp.com'
+  // const webhook = await bot.createWebhook({ domain: WEBHOOK_DOMAIN });
+  console.log('mek', `/telegraf/${bot.secretPathComponent()}`, 'port', process.env.PORT)
+
+//   app
+//   //   .post(`/telegraf/${bot.secretPathComponent()}`, (req, rep, ...rest) => {
+//   //   console.log(req, rep, ...rest)
+//   //   // return webhook(req.raw, rep.raw)
+//   // })
+//   .get('/', (...rest) => {
+//       console.log(rest)
+//      return `\
+// TelegaTimeBot
+// Solves YouTube in Telegram annoyances by getting video duration and converting timestamp.
+// Add:
+// https://t.me/telegatimebot
+//
+// source: https://github.com/mrv1k/telegatimebot
+// `;
+//     })
+//     .listen({ port: Number(process.env.PORT || 8080) })
+//     .then(() => console.log("Listening on port", Number(process.env.PORT || 8080)));
+  // app.post(`/telegraf/${bot.secretPathComponent()}`, webhook);
+
+  // app.post(`/telegraf/${bot.secretPathComponent()}`, async (...rest) => {
+  //   webhook()
+  // });
+
   bot
     .launch({
       webhook: {
-        domain: process.env.WEBHOOK_DOMAIN,
+        domain: WEBHOOK_DOMAIN,
         port: Number(process.env.PORT || 8080),
+        // path: '/bot'
+        // Optional path to listen for.
+        // `bot.secretPathComponent()` will be used by default
+        // path: webhookPath,
+
+        // Optional secret to be sent back in a header for security.
+        // e.g.: `crypto.randomBytes(64).toString("hex")`
+        // secretToken: randomAlphaNumericString,
       },
     })
     .then(() => {
       console.log("I am ALIVE! in " + process.env.NODE_ENV);
     });
-} else {
-  bot.launch().then(() => console.log("dev launch yolo"));
+  //
+  //
+  // app
+    // .listen({ port: Number(process.env.PORT || 8080) })
+    // .then(() => console.log("Listening on port", port));
 }
-
-// Enable graceful stop & kill
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
-
-// server is needed to keep bot alive?
-http
-  .createServer((_, res) => {
-    res.writeHead(200);
-    res.end(
-      `\
-TelegaTimeBot
-Solves YouTube in Telegram annoyances by getting video duration and converting timestamp.
-Add:
-https://t.me/telegatimebot
-
-source: https://github.com/mrv1k/telegatimebot
-`,
-
-    );
-  })
-  .listen(process.env.PORT || 8080);
+go()
