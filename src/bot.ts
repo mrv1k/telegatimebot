6a6e0cec1afae60c98c29e830130b5916afa8070
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2024-03-13 00:31:31 -0400

    heroku express
    Reluctance at first,
    ExpressJS and Heroku call,
    Begrudging embrace.

--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,7 @@ import { Telegraf } from "telegraf";
 import commands from "./commands";
 import errorHandler from "./errors";
 import { useNewReplies } from "telegraf/future";
+import express from "express";
 
 const { BOT_TOKEN, BOT_USERNAME = "telegatimebot" } = process.env;
 if (BOT_TOKEN === undefined) {
@@ -32,15 +33,32 @@ const go = async () => {
     throw new TypeError("NODE_ENV must be specified");
   }
 
-  bot
-    .launch({
-      webhook: {
-        domain: process.env.WEBHOOK_DOMAIN,
-        port: Number(process.env.PORT || 8080),
-      },
+  const path = `/telegraf/${bot.secretPathComponent()}`;
+  const telegaHook = await bot.createWebhook({
+    domain: process.env.WEBHOOK_DOMAIN,
+    path,
+  });
+
+  return express()
+    .get(path, telegaHook)
+    .post(path, telegaHook)
+    .get("/mek", (_, res) => res.type("text").send("mekmek"))
+    .get("/", (_, res) => {
+      res.setHeader("content-type", "text/plain").send(`\
+TelegaTimeBot
+Solves YouTube in Telegram annoyances by getting video duration and converting timestamp.
+Add:
+https://t.me/telegatimebot
+
+source: https://github.com/mrv1k/telegatimebot
+`);
     })
-    .then(() => {
-      console.log("I am ALIVE! in " + process.env.NODE_ENV);
+    .listen(Number(process.env.PORT) || 8080, () => {
+      console.log("I am ALIVE! in ", [
+        process.env.NODE_ENV,
+        Number(process.env.PORT) || 8080,
+      ]);
     });
-}
-go()
+};
+
+go();
