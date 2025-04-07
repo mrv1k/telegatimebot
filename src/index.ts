1ded9927f83b1e8376f1a07aeca59acea6e05cd4
Victor Voronov <victor@voronov.io>
2025-04-06 23:46:06 -0400

    chaos

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import commands from "./commands";
 import errorHandler from "./errors";
-import express from 'express';
+import { Hono } from "hono";
 import { Telegraf } from "telegraf";
 import { useNewReplies } from "telegraf/future";
 import { youtube } from "@googleapis/youtube";
 
-function configureBot(env: Env, youtubeClient: any) {
+import type { youtube_v3 } from "@googleapis/youtube";
+
+type Youtube = youtube_v3.Youtube;
+
+function configureBot(env: Env, youtubeClient: Youtube) {
   const bot = new Telegraf(env.BOT_TOKEN);
 
   bot.use(useNewReplies());
@@ -17,15 +21,16 @@ function configureBot(env: Env, youtubeClient: any) {
   process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
   // if (env.??? === "debug") {
+  // o
   //   bot.use(Telegraf.log());
   // }
-  return bot
+  return bot;
 }
 
 function configureYouTube(env: Env) {
   return youtube({
     version: "v3",
-    auth: env.YOUTUBE_API_KEY
+    auth: env.YOUTUBE_API_KEY,
   });
 }
 
@@ -34,31 +39,42 @@ function stayinAlive(env: Env) {
 }
 
 async function startProd(env: Env) {
-  const youtube = configureYouTube(env)
+  // const youtube = configureYouTube(env);
+  //
+  // const bot = configureBot(env, youtube);
+  // const path = `/telegraf/${bot.secretPathComponent()}`;
+  // const telegaHook = await bot.createWebhook({
+  //   domain: env.WEBHOOK_DOMAIN,
+  //   path,
+  // });
 
-  const bot = configureBot(env, youtube);
-  const path = `/telegraf/${bot.secretPathComponent()}`;
-  const telegaHook = await bot.createWebhook({
-    domain: env.WEBHOOK_DOMAIN,
-    path,
-  });
+  const hono = new Hono();
 
-  return express()
-    .get(path, telegaHook)
-    .post(path, telegaHook)
-    .get("/mek", (_, res) => res.type("text").send("mekmek"))
-    .get("/", (_, res) => {
-      res.setHeader("content-type", "text/plain").send(`\
-TelegaTimeBot
-Solves YouTube in Telegram annoyances by getting video duration and converting timestamp.
-Add:
-https://t.me/telegatimebot
+  return (
+    hono
+      // .get(path, telegaHook)
+      // .post(path, telegaHook)
+      .get("/", (c) => c.text("mekmek"))
+    //       .get("/", () => {
+    //         res.setHeader("content-type", "text/plain").send(`\
+    // TelegaTimeBot
+    // Solves YouTube in Telegram annoyances by getting video duration and converting timestamp.
+    // Add:
+    // https://t.me/telegatimebot
+    //
+    // source: https://github.com/mrv1k/telegatimebot
+    // `);
+    //       })
+    //       // TODO: figure out how to get the port?
+    //       // TODO: port to Hono
+    //       .listen(port, () => stayinAlive(port, env))
+  );
+}
+const app = new Hono();
 
-source: https://github.com/mrv1k/telegatimebot
-`);
-    })
-    .listen(port, () => stayinAlive(port, env));
-};
+app.get("/", (c) => c.text("Hello Cloudflare Workers!"));
+
+export default app;
 
 // async function toProdOrNotToProd(thatIsTheQuestion: boolean) {
 //   if (thatIsTheQuestion) {
@@ -70,12 +86,13 @@ source: https://github.com/mrv1k/telegatimebot
 //   }
 // }
 
-export default {
+const x = {
   async fetch(request, env, ctx): Promise<Response> {
     // process.env.NODE_ENV === 'production'
-    const youtubeClient = configureYouTube(env)
-    configureBot(env, youtubeClient)
+    const youtubeClient = configureYouTube(env);
+    // TODO: give bot access to youtube via middleware
+    configureBot(env, youtubeClient);
 
-    return new Response('Hello Vutyorld!');
+    return new Response("Hello Vutyorld!");
   },
 } satisfies ExportedHandler<Env>;
