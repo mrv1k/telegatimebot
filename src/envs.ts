c2759c3daa9cf329e8f93f6ca7f8e7baab100115
Victor Voronov <victor@voronov.io>
2025-04-07 23:58:13 -0400

    voices

--- src/envs.ts
+++ src/envs.ts
@@ -1,29 +1,31 @@
-const {
-  BOT_TOKEN,
-  BOT_USERNAME = "telegatimebot",
-  YOUTUBE_API_KEY,
-} = process.env;
+import "dotenv/config";
 
+export function parseEnv(data: unknown) {
+  const {
+    BOT_TOKEN,
+    BOT_USERNAME = "telegatimebot",
+    YOUTUBE_API_KEY,
+  } = process.env;
 
-// prod vars
-const PORT = Number(process.env.PORT) || 8080
+  // needed for any env
+  if (BOT_TOKEN === undefined) {
+    throw new TypeError("BOT_TOKEN must be provided");
+  }
+  if (YOUTUBE_API_KEY === undefined) {
+    throw new TypeError("YOUTUBE_API_KEY must be provided!");
+  }
+  process.title = BOT_USERNAME;
 
-if (BOT_TOKEN === undefined) {
-  throw new TypeError("BOT_TOKEN must be provided");
+  // needed for dev
+  if (process.env.NODE_ENV === undefined) {
+    throw new TypeError("NODE_ENV must be specified");
+  }
+
+  // needed for prod
+  // if (process.env.WEBHOOK_DOMAIN === undefined) {
+  //   throw new TypeError("NODE_ENV must be specified");
+  // }
+
+  // const PORT = Number(process.env.PORT) || 8080;
+  // process.env.WEBHOOK_DOMAIN;
 }
-if (YOUTUBE_API_KEY === undefined) {
-  throw new TypeError("YOUTUBE_API_KEY must be provided!");
-}
-
-if (process.env.WEBHOOK_DOMAIN === undefined) {
-  throw new TypeError("NODE_ENV must be specified");
-}
-
-
-// dev vars
-if (process.env.NODE_ENV === undefined) {
-  throw new TypeError("NODE_ENV must be specified");
-}
-process.title = BOT_USERNAME;
-
-process.env.WEBHOOK_DOMAIN,
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import commands from "./commands";
 import errorHandler from "./errors";
-import { Hono } from "hono";
 import { Telegraf } from "telegraf";
 import { useNewReplies } from "telegraf/future";
 import { youtube } from "@googleapis/youtube";
@@ -21,7 +20,6 @@ function configureBot(env: Env, youtubeClient: Youtube) {
   process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
   // if (env.??? === "debug") {
-  // o
   //   bot.use(Telegraf.log());
   // }
   return bot;
@@ -39,60 +37,28 @@ function stayinAlive(env: Env) {
 }
 
 async function startProd(env: Env) {
-  // const youtube = configureYouTube(env);
-  //
-  // const bot = configureBot(env, youtube);
+  const youtube = configureYouTube(env);
+
+  const bot = configureBot(env, youtube);
   // const path = `/telegraf/${bot.secretPathComponent()}`;
   // const telegaHook = await bot.createWebhook({
   //   domain: env.WEBHOOK_DOMAIN,
   //   path,
   // });
 
-  const hono = new Hono();
-
-  return (
-    hono
-      // .get(path, telegaHook)
-      // .post(path, telegaHook)
-      .get("/", (c) => c.text("mekmek"))
-    //       .get("/", () => {
-    //         res.setHeader("content-type", "text/plain").send(`\
-    // TelegaTimeBot
-    // Solves YouTube in Telegram annoyances by getting video duration and converting timestamp.
-    // Add:
-    // https://t.me/telegatimebot
-    //
-    // source: https://github.com/mrv1k/telegatimebot
-    // `);
-    //       })
-    //       // TODO: figure out how to get the port?
-    //       // TODO: port to Hono
-    //       .listen(port, () => stayinAlive(port, env))
-  );
+  // .get(path, telegaHook)
+  // .post(path, telegaHook)
+  return;
 }
-const app = new Hono();
 
-app.get("/", (c) => c.text("Hello Cloudflare Workers!"));
+async function toProdOrNotToProd(thatIsTheQuestion: boolean = true) {
+  if (thatIsTheQuestion) {
+    await startProd();
+  } else {
+    // development, debug or others
+    bot.launch();
+    stayinAlive();
+  }
+}
 
-export default app;
-
-// async function toProdOrNotToProd(thatIsTheQuestion: boolean) {
-//   if (thatIsTheQuestion) {
-//     await startProd();
-//   } else {
-//     // development, debug or others
-//     bot.launch();
-//     stayinAlive()
-//   }
-// }
-
-const x = {
-  async fetch(request, env, ctx): Promise<Response> {
-    // process.env.NODE_ENV === 'production'
-    const youtubeClient = configureYouTube(env);
-    // TODO: give bot access to youtube via middleware
-    configureBot(env, youtubeClient);
-
-    return new Response("Hello Vutyorld!");
-  },
-} satisfies ExportedHandler<Env>;
+toProdOrNotToProd(false);
